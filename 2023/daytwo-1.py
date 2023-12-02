c3d240837483ce3a0106c29412184a796f87e5d2
Alec Jensen <alecbradyjensen@gmail.com>
2023-12-02 10:04:00 -0600

    daw two part 1

---
+++ 2023/daytwo-1.py
@@ -0,0 +1,33 @@
+import re
+
+with open("daytwo.txt", "r") as f:
+    lines = [line.strip() for line in f.readlines()]
+
+limits = {
+    "red": 12,
+    "green": 13,
+    "blue": 14
+}
+
+total = 0
+
+for line in lines:
+    game_is_valid = True
+
+    game_header, line = line.split(": ")
+    game_number = int(game_header[5:])
+
+    cubes = re.split(r"; |, ", line)
+    
+    for cube in cubes:
+        num, cube = cube.split(" ")
+        
+        if int(num) > limits[cube]:
+            game_is_valid = False
+            break
+
+    if game_is_valid:
+        total += game_number
+
+print(total)
+    
